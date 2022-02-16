cd8c2dae6120ffab3ec2bf5fb62d06e3aca6e8ae
Julio <jucesargois@gmail.com>
2022-02-16 01:39:38 -0300

    Criado PetApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:clone_pet_app_figma/app/create_account/create_account_screen.dart';
 import 'package:clone_pet_app_figma/app/home/home_screen.dart';
+import 'package:clone_pet_app_figma/app/lets_go/lets_go_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const CreateAccountScreen());
+  runApp( PetApp());
 }
 
+class PetApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return const MaterialApp(
+      home: LetsGoScreen(),
+    );
+  }
+
+}
